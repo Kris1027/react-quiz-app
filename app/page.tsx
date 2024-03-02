7b82ef02d081f85c60b9f9c82d703bcdc9510651
Kris1027 <obarzanek.work@gmail.com>
2024-03-02 18:29:58 +0100

    new setup

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getQuestions } from "@/lib/questions";
-
-export default async function Home() {
-  const questions = await getQuestions();
-
-  console.log(questions);
-
-  return <main>start</main>;
-}
