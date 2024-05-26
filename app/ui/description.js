cac7096a6ec009c0276128583fc43569f649269c
Kris1027 <obarzanek.work@gmail.com>
2024-05-26 18:29:21 +0200

    better layout

--- app/ui/description.js
+++ app/ui/description.js
@@ -1,3 +1,5 @@
 export default function Description({ children }) {
-  return <h2 className="text-[var(--primaryText)] text-lg">{children}</h2>;
+  return (
+    <h2 className="text-[var(--primaryText)] text-lg font-bold">{children}</h2>
+  );
 }
--- app/views/startPage.js
+++ app/views/startPage.js
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import DifficultLevel from "../components/DifficultLevel";
 import Description from "../ui/description";
 
-export default function StartPage({ dispatch, timeLeft }) {
+export default function StartPage({ dispatch }) {
   return (
     <motion.div
       initial={{ opacity: 0, y: "-100vh" }}
@@ -23,8 +23,7 @@ export default function StartPage({ dispatch, timeLeft }) {
       <Description>
         Welcome in the quiz app where you can check your knowledge about React!
       </Description>
-      <div className="flex flex-col gap-4 lg:gap-0 lg:flex-row lg:justify-between items-center w-full">
-        <Description>Select Your Difficulty Level</Description>
+      <div className="flex flex-col gap-4 lg:flex-row w-full justify-between items-center">
         <DifficultLevel dispatch={dispatch} />
         <Button onClick={() => dispatch({ type: "start" })}>Start</Button>
       </div>
