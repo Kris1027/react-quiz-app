f0f52cb78a6e8aa4076cf1fd071a3f702cb607da
Kris1027 <obarzanek.work@gmail.com>
2024-06-03 18:05:24 +0200

    update favicon and easy difficult time change

--- app/components/DifficultLevel.js
+++ app/components/DifficultLevel.js
@@ -14,7 +14,7 @@ export default function DifficultLevel() {
         }
         className="p-1 text-md lg:text-lg font-bold bg-[var(--secondaryBackground)] text-[var(--primaryText)] transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px] active:scale-90 cursor-pointer uppercase outline-none"
       >
-        <option value={5}>Easy</option>
+        <option value={300}>Easy</option>
         <option value={250}>Medium</option>
         <option value={200}>Hard</option>
       </select>
